9a26496d898989257ae800500df8875be2484ced
Deon T. Benton <37884959+dtbenton@users.noreply.github.com>
2020-01-06 16:00:25 -0500

    s

--- neural_network/analysis/neural_network_analysis_in_R.R
+++ neural_network/analysis/neural_network_analysis_in_R.R
@@ -1,12 +1,12 @@
-########################################################
-########################################################
-########################################################
-#############                              #############
-#############     SOC ANALYSIS SCRIPT      #############
-#############                              #############
-########################################################
-########################################################
-########################################################
+############################################################
+############################################################
+############################################################
+#############                                  #############
+#############     NETWORK ANALYSIS SCRIPT      #############
+#############                                  #############
+############################################################
+############################################################
+############################################################
 # load all relevant libraries:
 library(lme4)
 library(nlme)
@@ -34,50 +34,27 @@ D = read.csv(file.choose(), header = TRUE)
 
 # get dimension of D
 dim(D)
-D = D[1:64,]
 
+# get structure of the D
+str(D)
 
 # convert relevant numerics to factors
-# sex
-D$sex = as.factor(D$sex)
 
-# effective_object_demonstrated_first
-D$effective_object_demonstrated_first = revalue(x = as.factor(D$effective_object_demonstrated_first), 
-                     c("0" = "No", "1"="Yes"))
-
-# feature_appended_to_correct_object
-D$feature_appended_to_correct_object = revalue(x = as.factor(D$feature_appended_to_correct_object), 
-                                                c("0" = "Circle", "1"="Diamond"))
-
-# correct_test_object_location
-D$correct_test_object_location = revalue(x = as.factor(D$correct_test_object_location), 
-                                         c("0" = "Left", "1"="Right"))
+# age
+D$age = revalue(x = as.factor(D$age), 
+                c("0" = "Older", "1"="Younger"))
 
 # test_choice
 D$test_choice = revalue(x = as.factor(D$test_choice), 
-                                         c("0" = "Incorrect", "1"="Correct"))
-
-# memory_check
-D$memory_check = revalue(x = as.factor(D$memory_check), 
                         c("0" = "Incorrect", "1"="Correct"))
-# age_cat
-D$age_cat = revalue(x = as.factor(D$age_cat), 
-                         c("0" = "Younger", "1"="Older"))
 
 
 # subset data to obtain two dataframes for 2 and 3 year olds #
-D.2yo = subset(D, ! age_cat %in% c("Older"))    # This creates a subsetted dataframe 
-                                        # containing only the data of the 2-year-olds
+D.2yo = subset(D, ! age %in% c("Older"))    # This creates a subsetted dataframe 
 
 
 
-D.3yo = subset(D, ! age_cat %in% c("Younger")) # This creates a subsetted dataframe 
-                                     # containing only the data of the 2-year-olds
-
-
-
-# reorder columns
-D = D[,c(9,1:8)]
+D.3yo = subset(D, ! age %in% c("Younger")) # This creates a subsetted dataframe 
 
 
 ####################
@@ -93,7 +70,7 @@ glm.global.boot = function(x,data){
   }
   glm.Bootobj = boot(data, glm.fit, R=5000)
   return(c(exp(glm.Bootobj$t0),exp(glm.Bootobj$t0)  + 1.96*c(-sd(glm.Bootobj$t), 
-                                         sd(glm.Bootobj$t))))
+                                                             sd(glm.Bootobj$t))))
 }
 
 
@@ -109,26 +86,26 @@ table(D$test_choice)
 # across all predictors
 baseline_sucess_prob = table(D$test_choice)[[2]]/(table(D$test_choice)[[1]]+table(D$test_choice)[[2]])
 baseline_success_odds = baseline_sucess_prob/(1-baseline_sucess_prob) # this is what will be shown
-                                                                      # as the overall 'odds' for a
-                                                                      # a model in which all the 
-                                                                      # predictors are set to '0'
-                                                                      # more precisely, the log of the
-                                                                      # odds will be what's reported by R
-                                                                      # I.e., log(baseline_success_odds)
+# as the overall 'odds' for a
+# a model in which all the 
+# predictors are set to '0'
+# more precisely, the log of the
+# odds will be what's reported by R
+# I.e., log(baseline_success_odds)
 
 # get baseline proportion (or odds) of success (i.e., 1/0+1), for
 # the two-year-olds but averaged across all predictors
-table(D$test_choice[D$age_cat=="Younger"])
-two_year_old_success_prob = table(D$test_choice[D$age_cat=="Younger"])[[2]]/(table(D$test_choice[D$age_cat=="Younger"])[[1]]+
-                                                                   table(D$test_choice[D$age_cat=="Younger"])[[2]])
+table(D$test_choice[D$age=="Younger"])
+two_year_old_success_prob = table(D$test_choice[D$age=="Younger"])[[2]]/(table(D$test_choice[D$age=="Younger"])[[1]]+
+                                                                   table(D$test_choice[D$age=="Younger"])[[2]])
 
 two_year_old_success_odds = two_year_old_success_prob/(1-two_year_old_success_prob) # this is what will be shown
-                                                                                    # as the overall 'odds' for a
-                                                                                    # a model in which all the 
-                                                                                    # predictors are set to '0'
-                                                                                    # more precisely, the log of the
-                                                                                    # odds will be what's reported by R
-                                                                                    # I.e., log(two_year_old_success_odds)
+# as the overall 'odds' for a
+# a model in which all the 
+# predictors are set to '0'
+# more precisely, the log of the
+# odds will be what's reported by R
+# I.e., log(two_year_old_success_odds)
 
 # plot two-year-old (baseline) distributions
 p.2y = ggplot(D.2yo, aes(test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
@@ -136,16 +113,16 @@ p.2y+geom_bar()
 
 # get baseline proportion (or odds) of success (i.e., 1/0+1), for
 # the three-year-olds but averaged across all predictors
-table(D$test_choice[D$age_cat=="Older"])
-three_year_old_success_prob = table(D$test_choice[D$age_cat=="Older"])[[2]]/(table(D$test_choice[D$age_cat=="Older"])[[1]]+
-                                                                     table(D$test_choice[D$age_cat=="Older"])[[2]])
+table(D$test_choice[D$age=="Older"])
+three_year_old_success_prob = table(D$test_choice[D$age=="Older"])[[2]]/(table(D$test_choice[D$age=="Older"])[[1]]+
+                                                                     table(D$test_choice[D$age=="Older"])[[2]])
 three_year_old_success_odds = three_year_old_success_prob/(1-three_year_old_success_prob) # this is what will be shown
-                                                                                          # as the overall 'odds' for a
-                                                                                          # a model in which all the 
-                                                                                          # predictors are set to '0'
-                                                                                          # more precisely, the log of the
-                                                                                          # odds will be what's reported by R
-                                                                                          # I.e., log(two_year_old_success_odds)
+# as the overall 'odds' for a
+# a model in which all the 
+# predictors are set to '0'
+# more precisely, the log of the
+# odds will be what's reported by R
+# I.e., log(two_year_old_success_odds)
 
 
 # plot three-year-old (baseline) distributions
@@ -185,16 +162,14 @@ anova(icc_mod1_3yo,null_model)
 # main analysis #
 #################
 # predictors/regressors #
-# effective_object_demonstrated_first
-# feature_appended_to_correct_object
-# correct_test_object_location
-# memory_check
+# age
+# condition
 
 ########################
 # response variable(s) #
 ########################
 # test_choice
-# memory_check
+
 
 # no data-subsetted, intercept-only model
 # this analysis examines the overall log odds of choosing
@@ -207,7 +182,7 @@ summary(glm.fit)
 # global analysis#
 ###############################
 names(D)
-main.glm.fit = glm(test_choice~(age_cat+memory_check)^2, data=D, family="binomial")
+main.glm.fit = glm(test_choice~(age+condition)^2, data=D, family="binomial")
 summary(main.glm.fit)
 Anova(main.glm.fit)
 
@@ -216,8 +191,7 @@ Anova(main.glm.fit)
 # 2-year-old analyses #
 #######################
 ## 2-yo data full model ##
-glm.fit.2yo = glm(test_choice~sex+condition+effective_object_demonstrated_first+
-                    feature_appended_to_correct_object+correct_test_object_location,
+glm.fit.2yo = glm(test_choice~condition,
                   data=D.2yo, family="binomial",
                   na.action = na.omit)
 summary(glm.fit.2yo)
@@ -226,7 +200,8 @@ Anova(glm.fit.2yo)
 xtabs(~test_choice, data = D.2yo)
 main.glm.fit.2yo = glm(test_choice~1, data=D.2yo, family="binomial")
 summary(main.glm.fit.2yo)
-glm.global.boot(8,D.2yo) # 95% C
+set.seed(2020)
+glm.global.boot(4,D.2yo) # 95% C
 
 
 # omnibus 2-yo figure
@@ -238,114 +213,14 @@ p.2yo.tc+geom_bar() + theme_bw() + # remove the gray background
   scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
   coord_cartesian(ylim=c(0, 20))
 
-##################################
-# follow-up analysis: 2-year-old #
-##################################
-# participants overall odds of responding correctly on the memory_check, 
-# where memory_check is the DV
-xtabs(~memory_check, data = D.2yo)
-glm.2yo.intercept.mem.odds = glm(memory_check ~ 1, data=D.2yo, 
-                               family = "binomial")
-summary(glm.2yo.intercept.mem.odds)
-exp(glm.2yo.intercept.mem.odds$coefficients)
-glm.global.boot(9,D.2yo) # 95% CI
-
-
-xtabs(~test_choice+memory_check,data=D.2yo)
-p.2yo = ggplot(D.2yo, aes(test_choice, fill = test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo+geom_bar() + theme_bw() + # remove the gray background
-  facet_wrap(~memory_check) +
-  scale_fill_manual(values= c("#FF9999","black")) +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 12))
-
-
-# follow-up comparisons to examine the marginally significant interaction: 2-year-olds #
-# comparing odds of success between 2-year-olds who passed memory check and those who did not #
-D.2yo.mcc = subset(D.2yo, ! memory_check %in% c("Incorrect"))
-D.2yo.mci = subset(D.2yo, ! memory_check %in% c("Correct"))
-dim(D.2yo.mcc)
-dim(D.2yo.mci)
-
-# model for 2-yo-mci
-D.2yo.mci.glm.fit = glm(test_choice ~ 1, data=D.2yo.mci, 
-                        family = "binomial")
-summary(D.2yo.mci.glm.fit)
-glm.global.boot(8,D.2yo.mci)
-
-
-# model for 2-yo mcc
-D.2yo.mcc.glm.fit = glm(test_choice ~ 1, data=D.2yo.mcc, 
-                        family = "binomial")
-summary(D.2yo.mcc.glm.fit)
-glm.global.boot(8,D.2yo.mcc)
-
-
-# subset data into memory-check(correct) and memory_check(incorrect) dataframes #
-D.mcc = subset(D, ! memory_check %in% c("Incorrect"))
-D.mci = subset(D, ! memory_check %in% c("Correct"))
-
-
-##########################################################
-# xtabs for the main effects for the 2-year-old analyses #
-##########################################################
-
-# effective_object_demonstrated_first
-xtabs(~effective_object_demonstrated_first+test_choice, data = D.2yo)
-fisher.test(xtabs(~effective_object_demonstrated_first+test_choice, data = D.2yo))
-
-# feature_appended_to_correct_object
-xtabs(~feature_appended_to_correct_object+test_choice, data = D.2yo)
-fisher.test(xtabs(~feature_appended_to_correct_object+test_choice, data = D.2yo))
-
-# correct_test_object_location
-xtabs(~correct_test_object_location+test_choice, data = D.2yo)
-fisher.test(xtabs(~correct_test_object_location+test_choice, data = D.2yo))
-
-# condition
-xtabs(~condition+test_choice, data = D.2yo)
-fisher.test(xtabs(~condition+test_choice, data = D.2yo))
-
-
-# overall counts for test_choice
-xtabs(~test_choice, data = D.2yo)
-chisq.test(xtabs(~test_choice, data = D.2yo), correct = FALSE)
-
-# overall counts for memory_check
-xtabs(~memory_check, data = D.2yo)
-chisq.test(xtabs(~memory_check, data = D.2yo), correct = FALSE)
-
-## 2-year-old plots ##
-# test_choice distribution
-p.2yo.tc = ggplot(D.2yo, aes(test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo.tc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 20)) 
-
-# memory_check distribution
-p.2yo.mc = ggplot(D.2yo, aes(memory_check)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 25))
 
 #######################################################################
 ############ SUMMARY '2-yo data-subsetted full model' #################
 #######################################################################
-# Although the 2-year-olds did not have a greater log odds of choosing 
-# the correct test object compared to the incorrect test object,
-# this is NOT because they failed to encode each individual
-# first-order relation. These results indicate that 2-year-olds
-# simply fail to detect second-order relations based on two, separate
-# first-order relations. 
-
-# It is also important to note that none of the other main effects,
-# which were subject to counterbalancing, were significant.
+# These analyses indicate that the younger networks, who correspond to 2-year-olds,
+# were as likely to respond correctly as they were to respond incorrectly at test.
+# This finding is in line with the data from the 2-year-olds who also were at chance
+# in their test choice. 
 ########################################################################
 
 
@@ -353,8 +228,7 @@ p.2yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray ba
 # 3-year-old analyses #
 #######################
 ## 3-yo data full model ##
-glm.fit.3yo = glm(test_choice~condition+effective_object_demonstrated_first+
-                    memory_check, 
+glm.fit.3yo = glm(test_choice~condition, 
                   data=D.3yo, family="binomial",
                   na.action = na.omit)
 
@@ -365,7 +239,7 @@ xtabs(~test_choice, data = D.3yo)
 glm.fit.3yo.intercept.tc = glm(test_choice ~ 1, data=D.3yo, 
                                family = "binomial")
 summary(glm.fit.3yo.intercept.tc)
-glm.global.boot(8,D.3yo) # 95% CI
+glm.global.boot(4,D.3yo) # 95% CI
 
 # omnibus 3-yo figure
 p.3yo.tc = ggplot(D.3yo, aes(test_choice, fill = test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
@@ -374,7 +248,7 @@ p.3yo.tc+geom_bar() + theme_bw() + # remove the gray background
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
   scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 20))
+  coord_cartesian(ylim=c(0, 25))
 
 
 
@@ -407,7 +281,7 @@ dim(D.3yo.mci)
 # participants overall odds of choosing the correc test object
 xtabs(~test_choice, data = D.3yo)
 glm.fit.3yo.mcc = glm(test_choice ~ 1, data=D.3yo.mcc, 
-                               family = "binomial")
+                      family = "binomial")
 summary(glm.fit.3yo.mcc)
 glm.global.boot(8,D.3yo.mcc)
 
@@ -461,9 +335,9 @@ p.3yo.tc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray ba
   scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
   coord_cartesian(ylim=c(0, 20)) +
   
-
-# memory_check distribution
-p.3yo.mc = ggplot(D.3yo, aes(memory_check)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
+  
+  # memory_check distribution
+  p.3yo.mc = ggplot(D.3yo, aes(memory_check)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
 p.3yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
---
+++ neural_network/random_number_generator_script_for_younger_network.R
@@ -0,0 +1,16 @@
+########################
+# RANDOM NUMBER SCRIPT #
+########################
+
+# Because the younger networks continually
+# produced equal, mid-range (~0.5) output activations for the consistent
+# and inconsistent test events, I used a random number generator to code
+# the younger network's test choices. Thus, a network was said to have
+# made a correct test choice if the random-number generator produced a '1'.
+# Otherwise, the network was said to ahve made an incorrect test choice and,
+# hence, assigned a '0'.
+
+set.seed(2245)
+rand_younger_net = sample(c(0,1),20, replace = TRUE)
+rand_younger_net
+table(rand_younger_net)
