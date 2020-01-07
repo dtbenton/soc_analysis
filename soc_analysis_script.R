1a04e19c4b2218625ff1418188971908ef503bb8
Deon T. Benton <37884959+dtbenton@users.noreply.github.com>
2020-01-07 15:07:22 -0500

    s

--- soc_analysis_script.R
+++ soc_analysis_script.R
@@ -34,7 +34,7 @@ D = read.csv(file.choose(), header = TRUE)
 
 # get dimension of D
 dim(D)
-D = D[1:64,]
+D = D[1:63,]
 
 
 # convert relevant numerics to factors
@@ -75,11 +75,6 @@ D.3yo = subset(D, ! age_cat %in% c("Younger")) # This creates a subsetted datafr
                                      # containing only the data of the 2-year-olds
 
 
-
-# reorder columns
-D = D[,c(9,1:8)]
-
-
 ####################
 ####################
 # global function  #
@@ -96,7 +91,16 @@ glm.global.boot = function(x,data){
                                          sd(glm.Bootobj$t))))
 }
 
-
+glm.global.boot_modified = function(x,z,data){
+  glm.fit = function(data,b,formula){ 
+    d= data[b,]
+    dif.1 =  glm(d[,x]~z, data=data, family = "binomial")
+    return(coef(dif.1))
+  }
+  glm.Bootobj = boot(data, glm.fit, R=5000)
+  return(c(exp(glm.Bootobj$t0[[2]]),exp(glm.Bootobj$t0[[2]])  + 1.96*c(-sd(glm.Bootobj$t), 
+                                                             sd(glm.Bootobj$t))))
+}
 
 #####################################
 # preliminary analyses and plotting #
@@ -212,21 +216,34 @@ summary(main.glm.fit)
 Anova(main.glm.fit)
 
 
-#######################
-# 2-year-old analyses #
-#######################
+#####################################
+# 2-year-old analyses: test choices #
+#####################################
 ## 2-yo data full model ##
-glm.fit.2yo = glm(test_choice~sex+condition+effective_object_demonstrated_first+
-                    feature_appended_to_correct_object+correct_test_object_location,
+glm.fit.2yo = glm(test_choice~sex+condition+correct_test_object_location,
                   data=D.2yo, family="binomial",
                   na.action = na.omit)
 summary(glm.fit.2yo)
 Anova(glm.fit.2yo)
-
+bf.2yo.sex = proportionBF(21, 11+21, p = .5) 
 xtabs(~test_choice, data = D.2yo)
 main.glm.fit.2yo = glm(test_choice~1, data=D.2yo, family="binomial")
 summary(main.glm.fit.2yo)
+
+set.seed(2020)
 glm.global.boot(8,D.2yo) # 95% C
+
+two.yo.sex.conting = xtabs(~test_choice+sex,data=D.2yo)
+bf.2yo.sex = contingencyTableBF(two.yo.sex.conting,
+                                sampleType = "indepMulti", 
+                                fixedMargin = "cols") # this analysis is to explore the marginally
+                                                      # significant effect of sex for the 2-year-olds.
+                                                      # This analysis indicated that male and females did
+                                                      # did not differ in their test choices.
+set.seed(2020)
+glm.global.boot_modified(9,D.2yo$sex,D.2yo) # this is supplementary to further show that there was no difference
+                                            # between the male and female infants given that the confidence interval
+                                            # contains 1. 
 
 
 # omnibus 2-yo figure
@@ -238,114 +255,50 @@ p.2yo.tc+geom_bar() + theme_bw() + # remove the gray background
   scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
   coord_cartesian(ylim=c(0, 20))
 
-##################################
-# follow-up analysis: 2-year-old #
-##################################
+
+
+#####################################
+# 2-year-old analyses: memory check #
+#####################################
 # participants overall odds of responding correctly on the memory_check, 
 # where memory_check is the DV
 xtabs(~memory_check, data = D.2yo)
 glm.2yo.intercept.mem.odds = glm(memory_check ~ 1, data=D.2yo, 
                                family = "binomial")
 summary(glm.2yo.intercept.mem.odds)
-exp(glm.2yo.intercept.mem.odds$coefficients)
+set.seed(2020)
 glm.global.boot(9,D.2yo) # 95% CI
 
 
+# examines whether participants who passed the memory check were more likely to 
+# choose the correct test object
 xtabs(~test_choice+memory_check,data=D.2yo)
-p.2yo = ggplot(D.2yo, aes(test_choice, fill = test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo+geom_bar() + theme_bw() + # remove the gray background
-  facet_wrap(~memory_check) +
-  scale_fill_manual(values= c("#FF9999","black")) +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 12))
+glm.2yo.mem.test.odds = glm(test_choice ~ memory_check, data=D.2yo, 
+                                 family = "binomial")
+summary(glm.2yo.mem.test.odds)
+Anova(glm.2yo.mem.test.odds)
+
+set.seed(2020)
+glm.global.boot(9,D.2yo) # 95% CI
 
 
-# follow-up comparisons to examine the marginally significant interaction: 2-year-olds #
-# comparing odds of success between 2-year-olds who passed memory check and those who did not #
-D.2yo.mcc = subset(D.2yo, ! memory_check %in% c("Incorrect"))
-D.2yo.mci = subset(D.2yo, ! memory_check %in% c("Correct"))
-dim(D.2yo.mcc)
-dim(D.2yo.mci)
-
-# model for 2-yo-mci
-D.2yo.mci.glm.fit = glm(test_choice ~ 1, data=D.2yo.mci, 
-                        family = "binomial")
-summary(D.2yo.mci.glm.fit)
-glm.global.boot(8,D.2yo.mci)
-
-
-# model for 2-yo mcc
-D.2yo.mcc.glm.fit = glm(test_choice ~ 1, data=D.2yo.mcc, 
-                        family = "binomial")
-summary(D.2yo.mcc.glm.fit)
-glm.global.boot(8,D.2yo.mcc)
-
-
-# subset data into memory-check(correct) and memory_check(incorrect) dataframes #
-D.mcc = subset(D, ! memory_check %in% c("Incorrect"))
-D.mci = subset(D, ! memory_check %in% c("Correct"))
-
-
-##########################################################
-# xtabs for the main effects for the 2-year-old analyses #
-##########################################################
-
-# effective_object_demonstrated_first
-xtabs(~effective_object_demonstrated_first+test_choice, data = D.2yo)
-fisher.test(xtabs(~effective_object_demonstrated_first+test_choice, data = D.2yo))
-
-# feature_appended_to_correct_object
-xtabs(~feature_appended_to_correct_object+test_choice, data = D.2yo)
-fisher.test(xtabs(~feature_appended_to_correct_object+test_choice, data = D.2yo))
-
-# correct_test_object_location
-xtabs(~correct_test_object_location+test_choice, data = D.2yo)
-fisher.test(xtabs(~correct_test_object_location+test_choice, data = D.2yo))
-
-# condition
-xtabs(~condition+test_choice, data = D.2yo)
-fisher.test(xtabs(~condition+test_choice, data = D.2yo))
-
-
-# overall counts for test_choice
-xtabs(~test_choice, data = D.2yo)
-chisq.test(xtabs(~test_choice, data = D.2yo), correct = FALSE)
-
-# overall counts for memory_check
-xtabs(~memory_check, data = D.2yo)
-chisq.test(xtabs(~memory_check, data = D.2yo), correct = FALSE)
-
-## 2-year-old plots ##
-# test_choice distribution
-p.2yo.tc = ggplot(D.2yo, aes(test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo.tc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 20)) 
-
-# memory_check distribution
-p.2yo.mc = ggplot(D.2yo, aes(memory_check)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.2yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 25))
 
 #######################################################################
 ############ SUMMARY '2-yo data-subsetted full model' #################
 #######################################################################
-# Although the 2-year-olds did not have a greater log odds of choosing 
-# the correct test object compared to the incorrect test object,
-# this is NOT because they failed to encode each individual
-# first-order relation. These results indicate that 2-year-olds
-# simply fail to detect second-order relations based on two, separate
-# first-order relations. 
+# This analysis indicated that not only were 2-year-olds as likely to 
+# choose the correct test object as they were to choose the incorrect 
+# test object, they were also as likely to pass the memory check as
+# they were not to pass the memory check. 
 
-# It is also important to note that none of the other main effects,
-# which were subject to counterbalancing, were significant.
+# In addition, although there was a marginally significant main effect
+# of sex, subsequent Bayes Factor analyses and bootstrapped CIs
+# indicate that this difference was not significant. Thus, this
+# main effect was not explored further. 
+
+# Finally, those who did pass the memory check were not more likely
+# to choose the correct test object than those who did not pass the 
+# memory check.
 ########################################################################
 
 
@@ -353,8 +306,7 @@ p.2yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray ba
 # 3-year-old analyses #
 #######################
 ## 3-yo data full model ##
-glm.fit.3yo = glm(test_choice~condition+effective_object_demonstrated_first+
-                    memory_check, 
+glm.fit.3yo = glm(test_choice~condition+memory_check, 
                   data=D.3yo, family="binomial",
                   na.action = na.omit)
 
@@ -365,26 +317,26 @@ xtabs(~test_choice, data = D.3yo)
 glm.fit.3yo.intercept.tc = glm(test_choice ~ 1, data=D.3yo, 
                                family = "binomial")
 summary(glm.fit.3yo.intercept.tc)
+set.seed(2020)
 glm.global.boot(9,D.3yo) # 95% CI
-
-# omnibus 3-yo figure
-p.3yo.tc = ggplot(D.3yo, aes(test_choice, fill = test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.3yo.tc+geom_bar() + theme_bw() + # remove the gray background
-  scale_fill_manual(values= c("#FF9999","black")) +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 25))
-
-
+bf.3yo.intercept = proportionBF(22,22+9,p=0.5) # this analysis indicates that 
+                                               # that there was overwhelming evidence
+                                               # in favor of the alternative hypothesis
+                                               # that 3-year-olds were more likely to choose
+                                               # the correct test object.
 
 
 # participants overall odds of responding correctly on the memory check
-xtabs(~memory_check, data = D.3yo)
+xtabs(~memory_check, data = D.3yo) # this analysis indicates that all but 1 3-year-old
+                                    # passed the memory check
 glm.fit.3yo.intercept.mc = glm(memory_check ~ 1, data=D.3yo, 
                                family = "binomial")
 summary(glm.fit.3yo.intercept.mc)
+set.seed(2020)
 glm.global.boot(9,D.3yo) # 95% CI
+bf.3yo.memory.check = proportionBF(30,30+1,p=0.5) # this analysis also indicated that there was overwhelming evidence
+                                                  # that the 3-year-olds were more likely to pass the memory check
+                                                  # than not to pass the memory check.
 
 
 # omnibus 3-yo memory-check figure
@@ -395,90 +347,18 @@ p.3yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray ba
   scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
   coord_cartesian(ylim=c(0, 25))
 
-
-
-## 3-year-old memory check subset analyses ##
-# subset data into memory-check(correct) and memory_check(incorrect) dataframes
-D.3yo.mcc = subset(D.3yo, ! memory_check %in% c("Incorrect"))
-D.3yo.mci = subset(D.3yo, ! memory_check %in% c("Correct"))
-dim(D.3yo.mcc)
-dim(D.3yo.mci)
-
-# participants overall odds of choosing the correc test object
-xtabs(~test_choice, data = D.3yo)
-glm.fit.3yo.mcc = glm(test_choice ~ 1, data=D.3yo.mcc, 
-                               family = "binomial")
-summary(glm.fit.3yo.mcc)
-glm.global.boot(8,D.3yo.mcc)
-bf.3yo = proportionBF(21, 11+21, p = .5) 
-
-# omnibus 3-yo memory-check figure
-xtabs(~test_choice, data = D.3yo.mcc)
-p.3yo.mc = ggplot(D.3yo.mcc, aes(test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.3yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 18))
-
-
-##########################################################
-# xtabs for the main effects for the 3-year-old analyses #
-##########################################################
-
-# effective_object_demonstrated_first
-xtabs(~effective_object_demonstrated_first+test_choice, data = D.3yo)
-fisher.test(xtabs(~effective_object_demonstrated_first+test_choice, data = D.3yo))
-
-# feature_appended_to_correct_object
-xtabs(~feature_appended_to_correct_object+test_choice, data = D.3yo)
-fisher.test(xtabs(~feature_appended_to_correct_object+test_choice, data = D.3yo))
-
-# correct_test_object_location
-xtabs(~correct_test_object_location+test_choice, data = D.3yo)
-fisher.test(xtabs(~correct_test_object_location+test_choice, data = D.3yo))
-
-# condition
-xtabs(~condition+test_choice, data = D.3yo)
-fisher.test(xtabs(~condition+test_choice, data = D.3yo))
-
-
-# overall counts for test_choice
-xtabs(~test_choice, data = D.3yo)
-chisq.test(xtabs(~test_choice, data = D.3yo), correct = FALSE)
-
-# overall counts for memory_check
-xtabs(~memory_check, data = D.3yo)
-chisq.test(xtabs(~memory_check, data = D.3yo), correct = FALSE)
-
-
-## 3-year-old plots ##
-# test_choice distribution
-p.3yo.tc = ggplot(D.3yo, aes(test_choice)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.3yo.tc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 20)) +
-  
-
-# memory_check distribution
-p.3yo.mc = ggplot(D.3yo, aes(memory_check)) # THE FIRST ARGUMENT VALUES AFTER 'AES' CORRESPONDS
-p.3yo.mc+geom_bar(fill=c("#FF9999","black")) + theme_bw() + # remove the gray background
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
-  scale_y_continuous(expand = c(0, 0)) + # ensure that bars hit the x-axis
-  coord_cartesian(ylim=c(0, 20))
 
 
 #######################################################################
 ############ SUMMARY '3-yo data-subsetted full model' #################
 #######################################################################
-# The analyses, at present, indicate that the 3-year-olds neither 
-# had a greater log odds of choosing the correct test object 
-# compared to the incorrect test object nor did they detect second-order 
-# relations based on two, separate first-order relations. 
+# The analyses indicates that, unlike the 2-year-olds, 3-year-olds were
+# both more likely to choose the correct test object and more likely to 
+# pass the memory check, which indicates that they encoded both the first-
+# and second-order relations between the objects, the features, and the 
+# machines' activation. 
 
-# This analysis should be excepted with a grain of salt given that
-# the full sample size (N = 32) has not been collected.
+# Together, these results indicate a developmental progression in children's
+# ability to detect SOCs involving an objects' internal feature and its
+# causal efficacy. 
 ########################################################################
