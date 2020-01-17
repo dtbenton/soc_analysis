997771097ae33c11853de12193c366ca9e10c441
Deon T. Benton <37884959+dtbenton@users.noreply.github.com>
2020-01-17 16:43:56 -0500

    s

---
+++ neural_network/model_parameter_manipulations/younger_network/SOC_cond_C_test.ex
@@ -0,0 +1,36 @@
+name: Consistent	
+I:	
+(Shape)                        
+ 0 0 0 0 0 0 0 0 1 1 1 1
+(Color)      
+ 0 0 1
+(Feature)    
+ 0 1
+T:	
+(ShapeOUT)                        
+ 0 0 0 0 0 0 0 0 1 1 1 1
+(ColorOUT)      
+ 0 0 1
+(FeatureOUT)    
+ 0 1
+(ActivityOUT)  
+ 1
+;
+name: Inconsistent	
+I:	
+(Shape)                        
+ 0 0 0 0 0 0 0 0 1 1 1 1
+(Color)      
+ 0 0 1
+(Feature)    
+ 1 0
+T:	
+(ShapeOUT)                        
+ 0 0 0 0 0 0 0 0 1 1 1 1
+(ColorOUT)      
+ 0 0 1
+(FeatureOUT)    
+ 1 0
+(ActivityOUT)  
+ 1
+;
